9af54b79247576baabe0d030c58b4e89c1464e14
support@unflab.com <support@unflab.com>
2023-08-24 09:49:12 +1000

    Simplify logic

---
+++ StrikethroughIssue/MainPage2.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Diagnostics;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace StrikethroughIssue
+{
+    public partial class MainPage2 : ContentPage
+    {
+        int count = 0;
+        public MainPage2()
+        {
+            InitializeComponent();
+        }
+
+        private void CounterBtn_Clicked(object sender, EventArgs e)
+        {
+            count++;
+            if (count == 1)
+                MyLabel.Text = $"Clicked {count} time";
+            else
+                MyLabel.Text = $"Clicked {count} times";
+        }
+
+        private void MyCheck_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            MyLabel.TextDecorations = MyCheck.IsChecked ? TextDecorations.Strikethrough : TextDecorations.Underline;
+            Debug.WriteLine(MyLabel.TextDecorations.ToString());
+        }
+    }
+
+}
